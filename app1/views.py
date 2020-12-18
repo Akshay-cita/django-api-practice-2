c1e84227d76c793be91e8685c06134e7d71844e9
Akshay-cita <edakkatakshay@gmail.com>
2020-12-18 18:10:40 +0530

    views and urls

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from djang.http import JsonResponse
+from .models import Poll
 # Create your views here.
+
+
+def polls_list(request):
+	max_objects=20
+	polls=Poll.objects.all()[:max_objects]
+	data={"result":list(polls.values("question","created_by__username","pub_date"))}
+	return JsonResponse(data)
+	
+
+
+
+
+def polls_detail(request,pk):
+	pass
